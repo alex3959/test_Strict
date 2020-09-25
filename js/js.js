efa921e6a60851453aa77a43256c5a3d18e6679d
alex3959 <akmal_kamolov.tj57@bk.ru>
2020-09-25 11:33:04 +0500

    links has changed

--- js/js.js
+++ js/js.js
@@ -6,10 +6,41 @@ $(function () {
   new WOW().init();
 })
 
+
+$(document).ready(function () {
+  $(".nav-list").on("click", "a", function (event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $("body, html").animate({ scrollTop: top }, 1000)
+  });
+
+  $(".btn_bottom").on("click", function (event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $("body, html").animate({ scrollTop: top }, 1000)
+  })
+
+})
+
+
+
+
 let navBtn = document.getElementById('nav-button');
 let listClose = document.querySelector('.list_close')
 let listWrap = document.getElementById('list-wrap');
-let navList = document.querySelector('.nav-list')
+let navList = document.querySelector('.nav-list');
+let navLinks = document.querySelectorAll('.nav-links');
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', () => {
+    listWrap.classList.remove('show');
+    listWrap.classList.add('hide');
+    document.body.style.overflow = 'scroll';
+  })
+}
+
 
 navBtn.onclick = function () {
   listWrap.classList.add('show')
